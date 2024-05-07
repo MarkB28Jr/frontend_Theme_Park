b4802b1ee6a9cf3a3b30ab6ab8f69b741f889ff2
MarkB28Jr <mark.brashear265@gmail.com>
2024-05-07 15:10:07 -0500

    park working

--- src/components/Park.jsx
+++ src/components/Park.jsx
@@ -2,27 +2,12 @@ import { useEffect, useState } from "react";
 import axios from 'axios'
 
 const Park = () => {
-  const [parks, setParks] = useState();
+  const [parks, setParks] = useState([]);
 
   const fetchParks = async () => {
     let response = await axios.get('https://imgainationland-f8738abfcd85.herokuapp.com/park')
     setParks(response.data)
   }
-
-  // const fetchparks = async () => {
-  //   try {
-  //     const response = await fetch("/park");
-  //     if (!response.ok) {
-  //       throw new Error("Network response was not ok");
-  //     }
-  //     const data = await response.json();
-  //     setparks(data);
-  //   } catch (error) {
-  //     setError(error.message);
-  //   } finally {
-  //     setLoading(false);
-  //   }
-  // };
 
   useEffect(() => {
     fetchParks();
@@ -30,10 +15,12 @@ const Park = () => {
 
   return (
     <div>
+      <input></input>
       {parks.map(park =>(
         <h3 key={park._id}> {park.name} </h3>
       ))}
     </div>
   )
 }
+
 export default Park
